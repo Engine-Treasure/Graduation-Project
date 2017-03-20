5d3e37292165dde1c8921d8a048f1d64e5b838fb
Engine Treasure <enginechen07@gmail.com>
2017-03-20 18:40:23 +0800

    20170320

--- src/main.py
+++ src/main.py
@@ -4,59 +4,87 @@ __author__ = "kissg"
 __date__ = "2017-03-10"
 
 import random
+from math import ceil
 
+import numpy as np
+import mingus.extra.lilypond as lp
 from deap import base
 from deap import tools
 from deap import creator
-from pysynth_e import make_wav
+from mingus.containers import Bar, Note, Track
+from mingus.midi import midi_file_out
 
-NOTES = [1, 1.5, 2, 2.5, 3, 4, 4.5, 5, 5.5, 6, 6.5, 7]  # 音符编码
-OCTAVE = [0, 1, 2, 3, 4, 5, 6, 7, 8]  # 八度编码
-DURATION = [1, 0.5, 0.25, 0.125, 0.0625, 0.03125, 0.015625]  # 时值编码, 最小 1/6
+from util import get_geometric_progression_of_2
+
+VELOCITY = {16, 33, 49, 64, 80, 96, 112, 126}
 
 # 适应度函数, `weights` weights 的每一项表示一个观测值
 # 暂定为 (1.0,), 表示最大化协和度
-creator.create("Fitness", base.Fitness, weights=(1.0,))
+creator.create("BarFitness", base.Fitness, weights=(1.0,))
+creator.create("TrackFitness", base.Fitness, weights=(1.0,))
 
-
-# [1, 5, 4] - C5 (scientific name) whose duration is 1/4
-creator.create("Note", list)
-
-
-# 以小节为个体, list 形式表示
-# meter: (4, 4), 四四拍, 前一个 4 表示以 4 分音符为一拍, 后一个 4 表示 4 拍为一个小节
+# 以小节为个体
 # [[1, 5, 4], [1.5, 5, 4], [3, 5, 4], [5, 5, 4]]
-creator.create("Individual", list, fitness=creator.Fitness, meter=tuple)
+creator.create("Bar", Bar, fitness=creator.BarFitness)
+creator.create("Track", Track, fitness=creator.TrackFitness)
 
 
-def get_note():
-    note = random.choice(NOTES)
+def gen_note(min=0, max=107):
+    '''
+    :param min: minimum scientific note name
+    :param max: maximum scientific note name
+    :return: mingus.containers.Note
+    '''
+    return Note().from_int(np.random.choice(range(min, max + 1)))
+
+
+def gen_duration(min=64, max=1):
+    '''
+    The parameters may be confusing. However 1 represents a whote note ,
+    2 a half note, 4 a quarter note, etc.
+    :return: a duration of note
+    '''
+    return np.random.choice(get_geometric_progression_of_2(max, min))
+
+
+def gen_bar(key="C", meter=(4, 4)):
+    bar = Bar(key, meter)
+    while not bar.is_full():
+        bar.place_notes(toolbox.note(),
+                        gen_duration(max=int(ceil(bar.value_left()))))
+    return bar
 
 
 
-def initial_individual():
+def evalute_bar(bar):
     pass
 
 
-def make_wav():
-    # 生成 wav 音乐
-    pass
-
-
-def evaluate(individual):
+def evaluate_track(individual):
     # Do some hard computing on the individual
     # Multi dimension
     return 1
+
+
+
+def gen_track(bars):
+    track = Track()
+    for bar in bars:
+        track.add_bar(bar)
+    return track
 
 
 toolbox = base.Toolbox()
 # 此处随机生成种群的个体
 # 注: toolbox.register(alias, func, args_for_func)
 # 注: tools.initRepeat(container, generator_func, repeat_times)
+toolbox.register("note", gen_note, min=9, max=96)  # 钢琴共 88 键, A0 ~ C8
+toolbox.register("bar", gen_bar, key="C", meter=(4, 4))
+toolbox.register("track", gen_track)
 
-ind1 = toolbox.gen_individual()
-ind1.fitness.values = evaluate(ind1)
-a = len(ind1) - 1
-song = [("".join(ind1[0:a]), ind1[a])] * 10
-print(song)
-make_wav(song, fn="a.wav")
+bars = [toolbox.bar() for i in range(1)]
+track = toolbox.track(bars)
+lp.to_pdf(lp.from_Track(track), "1.pdf")
+
+
+midi_file_out.write_Track("1.mid", track)
---
+++ src/util.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+__author__ = "kissg"
+__date__ = "2017-03-20"
+
+
+def get_geometric_progression_of_2(start, stop):
+    """
+    The usage is just like range(start, stop),
+    Because my range won't be large, I just use `range()` and `filter()` ,
+    instead of the the blow one: multiply and power.
+    :param start:
+    :param stop:
+    :param a:
+    :return:
+    """
+    return filter(lambda x: not (x - 1) & x, range(start, stop + 1))
+
+
+def get_geometric_progression_with_length(start, a, length):
+    return [start * a ** (n - 1) for n in range(1, length + 1)]
+
+if __name__ == '__main__':
+    print(get_geometric_progression_of_2(1, 64))
+    print(get_geometric_progression_with_length(1, 2, 10))
